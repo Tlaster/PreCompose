ff4743dab65fb072c63cf9bcc482cdaee1780759
Tlaster <tlaster@outlook.com>
2021-03-23 13:30:13 +0800

    add code style lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,37 @@ buildscript {
 group = "moe.tlaster"
 version = "0.1.0"
 
+plugins {
+    id("com.diffplug.spotless").version("5.6.1")
+}
+
 allprojects {
+    apply(plugin = "com.diffplug.spotless")
     repositories {
         jcenter()
         mavenCentral()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+}
+
+subprojects {
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+            ktlint("0.41.0")
+        }
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint("0.41.0")
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            allWarningsAsErrors = true
+            jvmTarget = "11"
+        }
     }
 }
