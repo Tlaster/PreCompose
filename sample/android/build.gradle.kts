4445368ebc115d646a852d24ce688e1793c952fc
Tlaster <tlaster@outlook.com>
2021-03-27 03:46:59 +0800

    upgrade to 0.4.0-build177

--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.compose") version "0.4.0-build176"
+    id("org.jetbrains.compose") version "0.4.0-build177"
     id("com.android.application")
     kotlin("android")
 }
