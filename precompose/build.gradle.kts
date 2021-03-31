36b0adaa89b8823fa0dc39d0d25deed43955bf30
Tlaster <tlaster@outlook.com>
2021-03-31 17:13:15 +0800

    add maven central publishing

--- precompose/build.gradle.kts
+++ precompose/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.compose.compose
+import java.util.Properties
 
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose") version "0.4.0-build177"
     id("com.android.library")
     id("maven-publish")
+    id("signing")
 }
 
 repositories {
@@ -69,5 +71,74 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+ext {
+    val publishPropFile = rootProject.file("publish.properties")
+    if (publishPropFile.exists()) {
+        Properties().apply {
+            load(publishPropFile.inputStream())
+        }.forEach { name, value ->
+            set(name.toString(), value)
+        }
+    } else {
+        set("signing.keyId", System.getenv("SIGNING_KEY_ID"))
+        set("signing.password", System.getenv("SIGNING_PASSWORD"))
+        set("signing.secretKeyRingFile", System.getenv("SIGNING_SECRET_KEY_RING_FILE"))
+        set("ossrhUsername", System.getenv("OSSRH_USERNAME"))
+        set("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
+    }
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    signing {
+        sign(publishing.publications)
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = if (version.toString().endsWith("SNAPSHOT")) {
+                uri(snapshotsRepoUrl)
+            } else {
+                uri(releasesRepoUrl)
+            }
+            credentials {
+                username = project.ext.get("ossrhUsername").toString()
+                password = project.ext.get("ossrhPassword").toString()
+            }
+        }
+    }
+    publications.withType<MavenPublication> {
+        artifact(javadocJar.get())
+        pom {
+            name.set("PreCompose")
+            description.set("A third-party Jetbrains Compose library with ViewModel, LiveData and Navigation support.")
+            url.set("https://github.com/Tlaster/PreCompose")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Tlaster")
+                    name.set("James Tlaster")
+                    email.set("tlaster@outlook.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/Tlaster/PreCompose")
+                connection.set("scm:git:git://github.com/Tlaster/PreCompose.git")
+                developerConnection.set("scm:git:git://github.com/Tlaster/PreCompose.git")
+            }
+        }
     }
 }
