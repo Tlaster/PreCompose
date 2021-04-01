0d98a0738e010aacd3343b89a26eff9f8b5d6b3d
Tlaster <tlaster@outlook.com>
2021-04-01 17:27:03 +0800

    add github actions

---
+++ .github/apply_signing.sh
@@ -0,0 +1,6 @@
+echo $SIGNING_KEY | base64 -d > key.gpg
+echo "signing.keyId=$SIGNING_KEY_ID
+signing.password=$SIGNING_PASSWORD
+signing.secretKeyRingFile=key.gpg
+ossrhUsername=$OSSRH_USERNAME
+ossrhPassword=$OSSRH_PASSWORD" >publish.properties
