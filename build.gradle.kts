7e4e3a3583b8810b05ec6add777efa8091ad480a
Tlaster <tlaster@outlook.com>
2021-04-09 18:03:50 +0800

    upgrade kotlin to 1.4.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.android.tools.build:gradle:4.1.1")
     }
 }
