e933b5b9bda6e2ea1f48922f6af5bbc4ee7d648a
Tlaster <tlaster@outlook.com>
2022-06-14 11:07:51 +0800

    upgrade dependency version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,14 +3,14 @@ import org.gradle.api.JavaVersion
 object Versions {
     object Android {
         const val min = 21
-        const val compile = 32
+        const val compile = 33
         const val target = compile
-        const val buildTools = "32.0.0"
+        const val buildTools = "33.0.0"
     }
 
     object Kotlin {
         const val lang = "1.6.21"
-        const val coroutines = "1.6.1"
+        const val coroutines = "1.6.2"
     }
 
     object Java {
@@ -18,9 +18,9 @@ object Versions {
         val java = JavaVersion.VERSION_11
     }
 
-    const val spotless = "6.5.0"
+    const val spotless = "6.7.0"
     const val ktlint = "0.45.2"
-    const val compose_jb = "1.2.0-alpha01-dev686"
+    const val compose_jb = "1.2.0-alpha01-dev709"
 
     object AndroidX {
         const val activity = "1.4.0"
