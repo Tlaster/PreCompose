1938126812e9f4f41ffca3deaa5ed1c705ef3276
Tlaster <tlaster@outlook.com>
2022-06-14 11:06:37 +0800

    upgrade agp version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0")
+    implementation("com.android.tools.build:gradle:7.2.1")
     api(kotlin("gradle-plugin", version = "1.6.21"))
 }
