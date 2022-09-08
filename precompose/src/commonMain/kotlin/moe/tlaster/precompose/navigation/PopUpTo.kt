7b7c9fa7eec782c23f1dc591d9e415606e426ed3
seiko <seiko_des@outlook.com>
2022-09-08 09:33:25 +0800

    refactor popUpTo

--- precompose/src/commonMain/kotlin/moe/tlaster/precompose/navigation/PopUpTo.kt
+++ precompose/src/commonMain/kotlin/moe/tlaster/precompose/navigation/PopUpTo.kt
@@ -1,12 +1,42 @@
 package moe.tlaster.precompose.navigation
 
-data class PopUpTo(
-    /**
-     * The `popUpTo` destination, if it's an empty string will clear all backstack
-     */
-    val route: String,
+sealed interface PopUpTo {
+
     /**
      * Whether the `popUpTo` destination should be popped from the back stack.
      */
-    val inclusive: Boolean = false
+    val inclusive: Boolean get() = false
+
+    /**
+     * do nothing
+     */
+    object None : PopUpTo
+
+    /**
+     * pop prev back stack
+     */
+    object Prev : PopUpTo
+
+    /**
+     * The `popUpTo` destination, if it's an empty string will clear all backstack
+     */
+    data class Route(
+        val route: String,
+        override val inclusive: Boolean,
+    ) : PopUpTo
+
+    companion object {
+
+        /**
+         * popUpTo first back stack
+         */
+        @Suppress("FunctionName")
+        fun First(inclusive: Boolean = true) : PopUpTo = Route("", inclusive)
+    }
+}
+
+@Suppress("FunctionName")
+fun PopUpTo(route: String, inclusive: Boolean = false) = PopUpTo.Route(
+    route = route,
+    inclusive = inclusive,
 )
