a7865f76cbaad2b14e710964f701b230448f4932
Sergei Gagarin <66745577+Lavmee@users.noreply.github.com>
2023-05-25 15:40:19 +0300

    fix: Sometimes the "collectAsStateWithLifecycle" function didn't work immediately (For example, this is very noticeable using with TextField). feat: Added "collectAsStateWithLifecycle" extension function for StateFlow. refactor: Changed the function "collectAsState" to "collectAsStateWithLifecycle" in the todo sample (NoteEditScene.kt).

--- precompose/src/commonMain/kotlin/moe/tlaster/precompose/flow/FlowExtensions.kt
+++ precompose/src/commonMain/kotlin/moe/tlaster/precompose/flow/FlowExtensions.kt
@@ -2,36 +2,64 @@ package moe.tlaster.precompose.flow
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.produceState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.withContext
 import moe.tlaster.precompose.lifecycle.Lifecycle
 import moe.tlaster.precompose.lifecycle.LocalLifecycleOwner
 import moe.tlaster.precompose.lifecycle.repeatOnLifecycle
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
+/**
+ * Collects values from this [StateFlow] and represents its latest value via [State] in a
+ * lifecycle-aware manner.
+ */
+
 @Composable
-fun <T : R, R> Flow<T>.collectAsStateWithLifecycle(
-    initial: R,
+fun <T : R, R> StateFlow<T>.collectAsStateWithLifecycle(
     context: CoroutineContext = EmptyCoroutineContext
 ): State<R> {
-    val lifecycleOwner = checkNotNull(LocalLifecycleOwner.current)
-    val flow = remember(this, lifecycleOwner) {
-        flowWithLifecycle(lifecycleOwner.lifecycle)
-    }
-    return flow.collectAsState(initial = initial, context = context)
+    return collectAsStateWithLifecycle(initial = this.value, context = context)
 }
 
-fun <T> Flow<T>.flowWithLifecycle(
+/**
+ * Collects values from this [Flow] and represents its latest value via [State] in a
+ * lifecycle-aware manner.
+ */
+
+@Composable
+fun <T> Flow<T>.collectAsStateWithLifecycle(
+    initial: T,
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> {
+    val lifecycleOwner = checkNotNull(LocalLifecycleOwner.current)
+    return collectAsStateWithLifecycle(
+        initial = initial,
+        lifecycle = lifecycleOwner.lifecycle,
+        context = context
+    )
+}
+
+/**
+ * Collects values from this [Flow] and represents its latest value via [State] in a
+ * lifecycle-aware manner.
+ */
+
+@Composable
+fun <T> Flow<T>.collectAsStateWithLifecycle(
+    initial: T,
     lifecycle: Lifecycle,
-): Flow<T> = callbackFlow {
-    lifecycle.repeatOnLifecycle {
-        this@flowWithLifecycle.collect {
-            send(it)
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> {
+    return produceState(initial, this, lifecycle, context) {
+        lifecycle.repeatOnLifecycle {
+            if (context == EmptyCoroutineContext) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            } else withContext(context) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            }
         }
     }
-    close()
 }
