2f384a0fe06dc78e41c184cbac209747b9ee8857
Tlaster <tlaster@outlook.com>
2023-07-18 17:10:59 +0800

    version 1.4.4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val precompose = "1.4.3"
+    const val precompose = "1.4.4"
     object Android {
         const val min = 21
         const val compile = 33
