65d74e744a71a4017449fb6feef2525ee0365dfb
Theron Rogers <theronrogers@gmail.com>
2023-07-27 17:23:23 -0600

    remove title parameter from PreComposeApplication in molecule sample

--- sample/molecule/src/uikitMain/kotlin/moe/tlaster/precompose/molecule/sample/Main.kt
+++ sample/molecule/src/uikitMain/kotlin/moe/tlaster/precompose/molecule/sample/Main.kt
@@ -34,7 +34,7 @@ class SkikoAppDelegate : UIResponder, UIApplicationDelegateProtocol {
 
     override fun application(application: UIApplication, didFinishLaunchingWithOptions: Map<Any?, *>?): Boolean {
         window = UIWindow(frame = UIScreen.mainScreen.bounds).apply {
-            rootViewController = PreComposeApplication("PreCompose Molecule Sample") {
+            rootViewController = PreComposeApplication {
                 App()
             }
             makeKeyAndVisible()
