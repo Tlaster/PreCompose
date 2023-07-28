4dbdb8d373c8946b1905e7de8ad547c614e5fc7d
Theron Rogers <theronrogers@gmail.com>
2023-07-27 18:06:34 -0600

    jetbrains compose verion to beta

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -22,7 +22,7 @@ object Versions {
     const val spotless = "6.7.0"
     const val ktlint = "0.45.2"
     const val compose = "1.5.0-beta03"
-    const val compose_jb = "1.5.0-dev1122"
+    const val compose_jb = "1.5.0-beta01"
 
     object AndroidX {
         const val activity = "1.7.2"
