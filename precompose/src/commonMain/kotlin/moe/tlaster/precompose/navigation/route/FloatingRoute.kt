e03b0f7e31d9621b67f1701d10b4226a7136bbc4
stylianosgakis <stylianos.gakis98@gmail.com>
2024-04-18 01:14:10 +0200

    Make scene provide an AnimatedContentScope to the composable content
    This scope is the one that drives the navigation transition, which
    allows callers to use it to drive other animations inside the screen
    itself as it animates into or out of the NavHost.
    Notably, this allows callers to use the shared element transitions.

--- precompose/src/commonMain/kotlin/moe/tlaster/precompose/navigation/route/FloatingRoute.kt
+++ precompose/src/commonMain/kotlin/moe/tlaster/precompose/navigation/route/FloatingRoute.kt
@@ -1,9 +1,18 @@
 package moe.tlaster.precompose.navigation.route
 
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
 import moe.tlaster.precompose.navigation.BackStackEntry
 
 internal class FloatingRoute(
-    override val content: @Composable (BackStackEntry) -> Unit,
+    override val content: @Composable AnimatedContentScope.(BackStackEntry) -> Unit,
     override val route: String,
-) : ComposeRoute, ComposeFloatingRoute
+) : ComposeRoute, ComposeFloatingRoute {
+    internal constructor(
+        route: String,
+        content: @Composable (BackStackEntry) -> Unit,
+    ) : this(
+        { entry -> content(entry) },
+        route,
+    )
+}
