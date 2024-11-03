76ed0665dd27205ef7b9e22bd8ad91cc99641c7b
Tlaster <tlaster@outlook.com>
2024-11-03 14:49:26 +0900

    update molecule sample

--- sample/molecule/composeApp/src/androidMain/kotlin/moe/tlaster/precompose/molecule/sample/MainActivity.kt
+++ sample/molecule/composeApp/src/androidMain/kotlin/moe/tlaster/precompose/molecule/sample/MainActivity.kt
@@ -3,7 +3,6 @@ package moe.tlaster.precompose.molecule.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
